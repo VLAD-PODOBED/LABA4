54933aaab707f2b6d191d5ce93fdcd84e0262a79
VLAD-PODOBED <podobedvladislavgeorg@gmail.com>
2024-03-10 10:51:54 +0300

    add

---
+++ LABA4/LABA4/15-01.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const contactRouter = require('./routers/contactRouter');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs',
+    helpers: {
+        goBack: () => 'window.location.href = \'/\''
+    }
+});
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+const app = express();
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use('/', contactRouter);
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`http://localhost:${port}`);
+    //console.log('https://lab04-express.herokuapp.com');
+});
+
+
